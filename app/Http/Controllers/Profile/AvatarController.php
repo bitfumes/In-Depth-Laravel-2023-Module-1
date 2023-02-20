036c9c856dff74efaedc57c7985f3400ee740599
sarthaksavvy <sarthak@bitfumes.com>
2023-02-20 09:02:58 +0530

    [main] 022 - Storage facade

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\UpdateAvatarRequest;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request)
     {
-        $path = $request->file('avatar')->store('avatars', 'public');
+        $path = Storage::disk('public')->put('avatars', $request->file('avatar'));
+        // $path = $request->file('avatar')->store('avatars', 'public');
+
+        if ($oldAvatar = $request->user()->avatar) {
+            Storage::disk('public')->delete($oldAvatar);
+        }
+
         auth()->user()->update(['avatar' => $path]);
 
         return redirect(route('profile.edit'))->with('message', 'Avatar is updated');
